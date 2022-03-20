91ccd0b1b0dd6c863d29a7d3aee3b44c5274b16e
Sandro Mejia <sandromejia3409@gmail.com>
2022-03-19 20:45:20 -0400

    completed note list comp and delete note function

--- src/components/MyNotes.js
+++ src/components/MyNotes.js
@@ -1,41 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import "../style/mynote.css";
+import NoteList from "./NoteList";
 
 const MyNotes = () => {
   const [title, setTitle] = useState("");
   const [note, setNote] = useState("");
-  const [list, setList] = useState("");
+  const [list, setList] = useState([]);
 
   const preventDefault = (e) => {
     e.preventDefault();
     console.log("clicked");
-    console.log(title, note);
 
-    if (title && note) {
-      const newNote = {
-        title: title,
-        note: note,
-      };
+    try {
+      if (title !== "" && note !== "") {
+        const newNote = {
+          id: new Date().getTime().toString(),
+          title: title,
+          note: note,
+        };
 
-      setList((e) => {
-        return [...e, newNote];
-      });
+        setList((e) => {
+          return [...e, newNote];
+        });
 
-      setTitle("");
-      setNote("");
-      console.log(list);
+        setTitle("");
+        setNote("");
+        console.log(list);
+      }
+    } catch (error) {
+      console.log(error);
+      console.log("note is blank");
     }
+  };
+
+  const deleteNote = (id) => {
+    const removeNote = list.filter((list) => list.id !== id);
+    setList(removeNote);
   };
 
   return (
     <>
       <p className="main-title">My notes</p>
-
       <form onSubmit={preventDefault}>
         <input
           type="text"
           id="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter note title..."
         />
         <br />
 
@@ -44,13 +56,20 @@ const MyNotes = () => {
           id="note"
           value={note}
           onChange={(e) => setNote(e.target.value)}
+          placeholder="Enter note..."
         />
+        <br />
         <br />
 
         <button className="add-note" type="submit">
           Add note
         </button>
       </form>
+
+      <br />
+      <br />
+
+      <NoteList arr={list} deleteFn={deleteNote} />
     </>
   );
 };
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,3 +1,24 @@
-const NoteList = ({ arr }) => {
-  return <></>;
+import "../style/notelist.css";
+const NoteList = ({ arr, deleteFn }) => {
+  return (
+    <>
+      {arr.map((note) => {
+        console.log(note.id);
+        return (
+          <div className="each-note" key={note.id}>
+            <div className="each-title">
+              <input type="text" value={note.title} />
+              <button onClick={() => deleteFn(note.id)}>Delete</button>
+            </div>
+            <br />
+            <input type="text" value={note.note} />
+            <br />
+            <br />
+          </div>
+        );
+      })}
+    </>
+  );
 };
+
+export default NoteList;
